1560d24a2bc7428a7459a241eff4af9044d14b78
Austin1094 <Austin.Adams1094@gmail.com>
2023-06-12 20:50:08 -0400

    Updated script.js to retrieve user input

--- script.js
+++ script.js
@@ -24,9 +24,29 @@ $(document).ready(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+
+  var currentHour = dayjs().hour();
+
+  $('.time-block').each(function () {
+    var timeBlockId = $(this).attr('id');
+
+    var hour = parseInt(timeBlockId.split('-')[1]);
+
+    if (hour < currentHour) {
+      $(this).addClass('past');
+    } else if (hour === currentHour) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+    // TODO: Add code to get any user input that was saved in localStorage and set
+    // the values of the corresponding textarea elements. HINT: How can the id
+    // attribute of each time-block be used to do this?
+    //
+
+    var userInput = localStorage.getItem(timeBlockId);
+
+    $(this).find('description').val(userInput);
+  });
   // TODO: Add code to display the current date in the header of the page.
 });
